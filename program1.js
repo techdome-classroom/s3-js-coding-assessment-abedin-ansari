8790581fe91ecb434a02769f4d7f3911cc2c2da6
abedin-ansari <jainulansari579@gmail.com>
2024-10-08 15:55:26 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,24 +2,6 @@
  * @param {string} s
  * @return {boolean}
  */
-function isValid(s) {
-  const stack = [];
-  const map = {
-    ")": "(",
-    "]": "[",
-    "}": "{",
-  };
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
-      stack.push(s[i]);
-    } else if (stack.length === 0 || map[s[i]] !== stack.pop()) {
-      return false;
-    }
-  }
-
-  return stack.length === 0;
-}
 // var isValid = function(s) {
 
 // };
