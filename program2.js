91b29efd49283a1cb5b6dbdcf304a10f74e33e7c
abedin-ansari <jainulansari579@gmail.com>
2024-10-08 16:07:26 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -15,7 +15,7 @@ var romanToInt = function(s) {
 
     let result = 0;
     for(let i=0; i< s.length; i++){
-        if(i+1 < s.length && map[s[i]] < map[s[i]])
+        if(i+1 < s.length && map[s[i]] < map[s[i+1]])
     }
 };
 
