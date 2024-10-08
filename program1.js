7deaa95e79224d79b701edd5f31964beb14a0737
abedin-ansari <jainulansari579@gmail.com>
2024-10-08 15:58:47 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,8 +11,8 @@ var isValid = function(s) {
     };
     for(let i = 0; i < s.length; i++){
         if(s[i] === '(' || s[i] === '[' || s[i] === '{'){
-            
-        }
+            stack.push(s[i]);
+        } else if(stack.length === 0)
     }
 };
 
